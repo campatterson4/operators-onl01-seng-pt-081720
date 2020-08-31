2a44e1822c6998c19884dc852a81c9b60ff8c220
Camryn Patterson <camrynpatterson@yahoo.com>
2020-08-31 02:12:08 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,18 +1,11 @@
-# Example-1
-var = 5;
-var_is_greater_than_three = (var > 3 ? true : false);  
-puts var_is_greater_than_three 
+def unsafe?(speed)
+	if speed > 60 || speed < 40
+		return true;
+	else
+		return false;
+	end
+end
 
-# Example-2
-score= 50
-result = score > 40 ? 'Pass' : 'Fail'
-puts result
- 
-# Example-3
-score = 10;
-age = 22;
-puts "Taking into account your age and score, you are : ",(age > 10 ? (score < 80 ? 'behind' : 'above average') : (score < 50 ? 'behind' : 'above average')); 
-
-# Example-4
-score = 81
-puts "Based on your score, you are a ", (score > 80 ? "genius" :  "Not genius")
+def not_safe?(speed)
+	speed > 60 || speed < 40 ? true : false;
+end
